4ddbb88bf6ba074e2a15e6cbd63db0b366d3d151
Philippe MICHEL <philippe.michel1@gmail.com>
2024-04-16 15:44:58 +0200

    Initial Commit

---
+++ sunlight/sunlightApp.swift
@@ -0,0 +1,17 @@
+//
+//  sunlightApp.swift
+//  sunlight
+//
+//  Created by Philippe MICHEL on 16/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct sunlightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
